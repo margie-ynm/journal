61a02ae4bd2036dd3c5bba672df0736a42884e02
Marty Kovach and Margie Nevarez <margie@>
2017-01-02 13:37:29 -0800

    add working word count method.

---
+++ js/entry.js
@@ -0,0 +1,14 @@
+function Entry(title, content) {
+  this.title = title;
+  this.content = content;
+};
+
+
+Entry.prototype.wordCount = function (content) {
+  var count = 0;
+  var words = content.split(" ");
+    words.forEach(function(word){
+     count += 1;
+  })
+return count;
+};
